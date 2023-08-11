2dc96afd11a4510601a9c4f55b94ae4b1a6ec229
LILANGYU <1577958961@qq.com>
2023-08-11 14:31:00 +0800

    add_yahboom_360-servo

--- main.ts
+++ main.ts
@@ -290,6 +290,35 @@ namespace SuperBitV2 {
         // 50hz: 20,000 us
         
         if (pos == enPos.stop) {
+            let us = (86 * 1800 / 180 + 600); // 0.6 ~ 2.4 
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+        else if(pos == enPos.forward){ //0-90 -> 90 - 0
+            let us = ((90-value) * 1800 / 180 + 600); // 0.6 ~ 2.4 
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+        else if(pos == enPos.reverse){ //0-90 -> 90 -180  
+            let us = ((90+value) * 1800 / 180 + 600); // 0.6 ~ 2.4
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+
+       
+
+    }
+
+    //% blockId=SuperBitV2_Servo4 block="Servo(360°_rotatable)|num %num|pos %pos|value %value"
+    //% weight=96
+    //% blockGap=10
+    //% num.min=1 num.max=4 value.min=0 value.max=90
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=20
+    export function Servo4(num: enServo, pos: enPos, value: number): void {
+
+        // 50hz: 20,000 us
+        
+        if (pos == enPos.stop) {
             let us = (110 * 1800 / 180 + 600); // 0.6 ~ 2.4 error:86->110
             let pwm = us * 4096 / 20000;
             setPwm(num, 0, pwm);
